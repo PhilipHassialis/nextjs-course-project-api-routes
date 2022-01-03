6b6c71f9b15249910104cc2d57ae333608b4eb18
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-01-03 15:49:43 +0200

    Running MongoDB Queries From Inside API Routes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -6,6 +8,13 @@ const handler = (req, res) => {
       res.status(422).json({ message: "Invalid email address" });
       return;
     }
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://NEXTJSUSER:aiD0ppjwUKorq8pB@cluster0.tkycs.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    await db.collection("emails").insertOne({ email: userEmail });
+    client.close();
 
     console.log(userEmail);
     res.status(201).json({ message: "Signed up" });
