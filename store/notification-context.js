e2f7af30f01982e1b214db42ec6fe21ae3e13ed3
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-01-05 10:20:27 +0200

    Example: Removing Notifications (Automatically)

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NotificationContext = createContext({
   notification: null, // title, message, status
@@ -8,6 +8,20 @@ const NotificationContext = createContext({
 
 export const NotificationContextProvider = (props) => {
   const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      ["success", "error"].includes(activeNotification.status)
+    ) {
+      const timer = setTimeout(() => {
+        hideNotificationHandler();
+      }, 3000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
 
   const showNotificationHandler = (notificationData) => {
     setActiveNotification(notificationData);
